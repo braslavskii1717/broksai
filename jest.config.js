ef4e6e7e8163e059cad7a1eb95dec07baebe13be
braslavskii1717 <braslavskii1717@gmail.com>
2025-11-20 16:23:37 +0400

    feat: Add Jest configuration file
    - Configure ts-jest preset for TypeScript support
    - Set test environment to node
    - Configure test roots and match patterns
    - Add coverage collection settings

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/__tests__/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
